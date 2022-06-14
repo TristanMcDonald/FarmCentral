6fe40896470567e331d0a9df02e450349c37ba23
Tristan McDonald <tristan.cole.mcdonald@gmail.com>
2022-06-14 13:55:55 +0200

    Changed startup page and added Products link to navbar

--- Startup.cs
+++ Startup.cs
@@ -50,7 +50,7 @@ namespace FarmCentral
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Farmer}/{action=Login}/{id?}");
             });
         }
     }
