711320773e8babe5d4b51dca8c95ca34210dc583
Tristan McDonald <tristan.cole.mcdonald@gmail.com>
2022-06-14 20:30:28 +0200

    Implemented a Farmer_Product controller and Data Access layer with methods to get a list of all farmers and view specific farmers products

---
+++ Controllers/Farmer_ProductController.cs
@@ -0,0 +1,43 @@
+﻿using FarmCentral.DataAccess;
+using FarmCentral.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FarmCentral.Controllers
+{
+    public class Farmer_ProductController : Controller
+    {
+        //GET: Farmer_ProductDAL
+        //Creating an object of the ProductDAL
+        public Farmer_ProductDAL farmer_ProductDAL = new Farmer_ProductDAL();
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult AllFarmers()
+        {
+            //Get all Farmers
+            var farmerList = new List<Farmer>();
+            //Calling the DAL method which parses a list with all farmers
+            farmerList = farmer_ProductDAL.GetAllFarmers().ToList();
+            //Return the list of farmers.
+            return View(farmerList);
+        }
+
+        //Get Farmers Products
+        [Route("Farmer_Product/Products/{Username}")]
+        [HttpGet]
+        public IActionResult FarmersProducts(string Username)
+        {
+            //Get all Products that belong to the selected Farmer
+            var farmersProductList = new List<Farmer_Product>();
+            //Calling the DAL method which parses a list with all the farmers products
+            farmersProductList = farmer_ProductDAL.GetSpecificFarmersProducts(Username).ToList();
+            //Return the list of farmers products.
+            return View(farmersProductList);
+        }
+    }
+}
---
+++ DataAccess/Farmer_ProductDAL.cs
@@ -0,0 +1,55 @@
+﻿using FarmCentral.Data;
+using FarmCentral.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FarmCentral.DataAccess
+{
+    public class Farmer_ProductDAL
+    {
+        private FarmCentralContext _context;
+
+        public Farmer_ProductDAL()
+        {
+            _context = new FarmCentralContext();
+        }
+
+        //Fetching all farmers from the database.
+        public IEnumerable<Farmer> GetAllFarmers()
+        {
+            //initializing the farmersProducts list.
+            List<Farmer> farmers = new List<Farmer>();
+
+            foreach (var Farmer in _context.Farmers)
+            {
+                farmers = new List<Farmer>
+                    (from f in _context.Farmers
+                     select f
+                    );
+            }
+            return farmers;
+        }
+
+        //Fetching all products assigned to all farmers.
+        public IEnumerable<Farmer_Product> GetSpecificFarmersProducts(string Username)
+        {
+
+            //initializing the farmersProducts list.
+            List<Farmer_Product> allFarmersProducts = new List<Farmer_Product>();
+
+            foreach (var FarmerProduct in _context.Farmer_Products)
+            {
+                allFarmersProducts = new List<Farmer_Product>
+                    (from fp in _context.Farmer_Products
+                     join f in _context.Farmers
+                     on fp.FarmerUsername equals f.Username
+                     join p in _context.Products
+                     on fp.ProductName equals p.Name
+                     where fp.FarmerUsername.Equals(Username)
+                     select fp
+                    );
+            }
+            return allFarmersProducts;
+        }
+    }
+}
