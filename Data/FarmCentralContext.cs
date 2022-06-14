4c1f2f9a07c3c79ba5c1cd9f51a9ce9939bbd855
Tristan McDonald <tristan.cole.mcdonald@gmail.com>
2022-06-14 12:54:28 +0200

    Added FarmCentral Database Context

---
+++ Data/FarmCentralContext.cs
@@ -0,0 +1,17 @@
+﻿using FarmCentral.Models;
+using System.Data.Entity;
+
+namespace FarmCentral.Data
+{
+    public class FarmCentralContext : DbContext
+    {
+        public FarmCentralContext()
+        {
+        }
+
+        public DbSet<Farmer> Farmers { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Farmer_Product> Farmer_Products { get; set; }
+    }
+}
