56cde864082c67e8532722a03ff0808f7b1ad113
Tristan McDonald <tristan.cole.mcdonald@gmail.com>
2022-06-15 15:02:55 +0200

    Implemented functionality for an employee to add a new farmer to the database (Added AddFarmer view and controller method).

--- Controllers/Farmer_ProductController.cs
+++ Controllers/Farmer_ProductController.cs
@@ -9,8 +9,12 @@ namespace FarmCentral.Controllers
     public class Farmer_ProductController : Controller
     {
         //GET: Farmer_ProductDAL
-        //Creating an object of the ProductDAL
+        //Creating an object of the Farmer_ProductDAL
         public Farmer_ProductDAL farmer_ProductDAL = new Farmer_ProductDAL();
+
+        //GET: Farmer_ProductDAL
+        //Creating an object of the FarmerDAL
+        public FarmerDAL farmerDAL = new FarmerDAL();
 
         public IActionResult Index()
         {
@@ -38,6 +42,26 @@ namespace FarmCentral.Controllers
             farmersProductList = farmer_ProductDAL.GetSpecificFarmersProducts(Username).ToList();
             //Return the list of farmers products.
             return View(farmersProductList);
+        }
+
+        [HttpGet]
+        public IActionResult AddFarmer()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult AddFarmer([Bind] Farmer farmerObj)
+        {
+            if (ModelState.IsValid)
+            {
+                //Calling the DAL method to add a new farmer to the database.
+                farmerDAL.RegisterNewFarmer(farmerObj);
+                //once the farmer is added redirect to the all farmers view.
+                return RedirectToAction("AllFarmers");
+            }
+            return View(farmerObj);
         }
     }
 }
